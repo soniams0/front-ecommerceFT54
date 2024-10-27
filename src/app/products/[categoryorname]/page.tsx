606c607875df7d7f267d276687fd133340174b22
soniams0 <sony16mun@gmail.com>
2024-10-26 21:30:12 -0500

    volver categoryorname a server component

--- src/app/products/[categoryorname]/page.tsx
+++ src/app/products/[categoryorname]/page.tsx
@@ -1,41 +1,26 @@
-'use client'
 import Card from '@/components/Card/Card';
 import { getProductsByCategoryOrName } from '@/helpers/products.helper';
 import { IProduct } from '@/interfaces';
 import Link from 'next/link';
-import React, { useEffect, useState } from 'react';
+import React from 'react'
 
-const Products: React.FC<{ params: { categoryorname: string } }> = ({ params }) => {
-    const [products, setProducts] = useState<IProduct[]>([]);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        const fetchProducts = async () => {
-            if (params.categoryorname) {
-                const fetchedProducts = await getProductsByCategoryOrName(params.categoryorname);
-                setProducts(fetchedProducts);
-            }
-            setLoading(false);
-        };
-
-        fetchProducts();
-    }, [params.categoryorname]);
-
-    if (loading) return <div>Loading...</div>;
-
+const Products: React.FC <{params: {categoryorname: string}}> = async ({params}) => {
+  
+    const products = await getProductsByCategoryOrName(params.categoryorname)
     return (
-        <div className="flex flex-wrap items-center gap-4 p-4 justify-center ">
-            {products.length ? (
-                products.map((product: IProduct) => (
-                    <Link key={product.id} href={`/product/${product.id}`}>
-                        <Card {...product} />
-                    </Link>
-                ))
-            ) : (
-                <div>Products not found</div>
-            )}
-        </div>
-    );
+      <div className="flex flex-wrap items-center gap-4 p-4 justify-center ">
+          {products.length ? (
+              products?.map((product: IProduct) => {
+                  return (
+                      <Link key={product.id} href={`/product/${product.id}`}>
+                          <Card {...product} />
+                      </Link>
+                  );
+              })
+          ) : (
+              <div>Products not found</div>
+          )}
+      </div>
+  );
 };
-
-export default Products;
+export default Products
